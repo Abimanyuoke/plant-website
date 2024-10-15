ca9222f7d6cfc7303814c17e9ec3adca860f9332
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-10-15 21:59:50 +0700

    add footer, add content, add media social, and add scroll up, and then scroll header

--- public/index.js
+++ public/index.js
@@ -15,6 +15,34 @@ navLink.forEach(link => {
     })
 })
 
+// show scroll up
+const scrollUp = () => {
+  const scrollUpBtn = document.getElementById("scroll-up")
+
+  if(this.scrollY >= 205) {
+    scrollUpBtn.classList.remove("-bottom-1/2")
+    scrollUpBtn.classList.add("bottom-4")
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2")
+    scrollUpBtn.classList.remove("bottom-4")
+  }
+}
+
+// change background header
+const scrollHeader = () => {
+  const header = document.getElementById("navbar")
+
+  if(this.scrollY >= 205) {
+   header.classList.add("border-b", "border-yellow-500")
+  } else {
+   header.classList.remove("border-b", "border-yellow-500")
+  }
+}
+
+
+
+window.addEventListener("scroll", scrollHeader)
+
 // swiper
 const swiper = new Swiper('.swiper', {
   speed: 400,
